661b3d2960e2ab98828451e86fe28fdccc91e970
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2025-12-28 10:59:37 -0500

    Change dataframe width to 'stretch' in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data = smoothiefroot_response.json(), width = True)
+st_df = st.dataframe(data = smoothiefroot_response.json(), width='stretch')
