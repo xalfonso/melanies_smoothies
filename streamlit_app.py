c8e69525c84d4a6a33e849490e6aedb6381800b8
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2025-12-28 12:08:35 -0500

    Refactor dataframe conversion and API call
    Updated dataframe handling and API request in streamlit_app.py.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,13 @@ st.write('The name on your smoothie will be', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the snowpark dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
@@ -35,8 +41,12 @@ if ingredients_list:
 
   for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
+
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+    
     st.subheader(fruit_chosen + ' Nutrition Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
     st_df = st.dataframe(data = smoothiefroot_response.json(), width='stretch')
     
 
