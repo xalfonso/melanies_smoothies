57f23a3e8e7032206b9d04c80a8ed7b3e5704d40
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2025-12-28 11:00:43 -0500

    Fix import order and update requests usage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
 
 
 # Write directly to the app
@@ -44,7 +43,7 @@ if ingredients_list:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-
+import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 st_df = st.dataframe(data = smoothiefroot_response.json(), width='stretch')
